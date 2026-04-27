72c27e227ecc176ff6a86874206450017b0cfc66
Anthony GARGOUA <56581894+AnthonyGargoua@users.noreply.github.com>
2026-04-27 12:20:31 +0200

    layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import './globals.css'
 
 export const metadata = {
-  title: 'Darts 301 Tracker',
+  title: 'Fléchettes entre copains',
   description: 'Application interactive de fléchettes',
 }
 
